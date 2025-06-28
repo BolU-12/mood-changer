dbfded3270cc034ce6685c68f4d922eee28d00e1
Darasimi Nelson <167296301+BolU-12@users.noreply.github.com>
2025-06-28 15:01:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let sademoji= document.getElementById("sademoji");
+let happyemoji=document.getElementById( "happyemoji");
+
+sademoji.addEventListener("click", function(){ 
+    imager.style.display="flex";
+});
+
+imager.addEventListener("click",function(){ 
+    imager.style.display="none";
+})
